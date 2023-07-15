97d3358e8cdbaab166228ced262f2459a499a938
saiashish8364 <polusaiashish97@gmail.com>
2023-07-15 19:00:10 +0530

    up1

---
+++ shopping-cart/src/components/Cart/CartButton.js
@@ -0,0 +1,18 @@
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../Store/ui-slice";
+import { useDispatch } from "react-redux";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+  return (
+    <button onClick={toggleCartHandler} className={classes.button}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
